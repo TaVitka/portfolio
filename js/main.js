a7e4858e2f9a0ccb799537b758eac1fee1a40cc8
TaVitka <tavitka17@gmail.com>
2025-02-13 23:57:35 +0100

    added fixes for styles on mobile devices, added fixes for burger menu in js

--- js/main.js
+++ js/main.js
@@ -1,67 +1,70 @@
 document.addEventListener('DOMContentLoaded', function(){
   //header
-  
+
+  const page = document.querySelector('body');
   const pageHeader = document.querySelector('.header');
-    const pageFooter = document.querySelector('.footer');
+  const pageFooter = document.querySelector('.footer');
+  const pageOverlay = document.querySelector('.overlay');
 
-    const startHeightValue = () => {
-        let vh = window.innerHeight * 1;
-        document.querySelector(':root').style.setProperty('--vh', `${vh}px`);
+  const startHeightValue = () => {
+    let vh = window.innerHeight * 1;
+    document.querySelector(':root').style.setProperty('--vh', `${vh}px`);
 
-        const headerHeight = pageHeader ? pageHeader.offsetHeight : 0;
-        const footerHeight = pageFooter ? pageFooter.offsetHeight : 0;
+    const headerHeight = pageHeader ? pageHeader.offsetHeight : 0;
+    const footerHeight = pageFooter ? pageFooter.offsetHeight : 0;
 
-        document.documentElement.style.setProperty(
-            '--header-height',
-            `${headerHeight}px`,
-        );
-        document.documentElement.style.setProperty(
-            '--footer-height',
-            `${footerHeight}px`,
-        );
-    };
-
-    window.addEventListener('resize', startHeightValue);
-    window.addEventListener('scroll', startHeightValue);
-
-    startHeightValue();
-
-    let headerHeight = parseFloat(
-        document.documentElement.style.getPropertyValue('--header-height')
+    document.documentElement.style.setProperty(
+      '--header-height',
+      `${headerHeight}px`,
     );
 
-    let lastHeaderPosition;
-    let newHeaderPosition;
+    document.documentElement.style.setProperty(
+      '--footer-height',
+      `${footerHeight}px`,
+    );
+  };
 
-    const hideHeaderOnScroll = () => {
-        lastHeaderPosition = window.scrollY;
-        addClassItem('.header', 'scroll');
+  window.addEventListener('resize', startHeightValue);
+  window.addEventListener('scroll', startHeightValue);
 
-        if (headerHeight - 50 < lastHeaderPosition && lastHeaderPosition > newHeaderPosition && newHeaderPosition !== 0) {
-            addClassItem('.header', 'hide');
-            addClassItem('.header', 'scroll');
-        } else if ((newHeaderPosition > lastHeaderPosition) && lastHeaderPosition !== 0 || lastHeaderPosition < headerHeight) {
-            removeClassItem('.header', 'hide');
+  startHeightValue();
+
+  let headerHeight = parseFloat(
+    document.documentElement.style.getPropertyValue('--header-height')
+  );
+
+  let lastHeaderPosition;
+  let newHeaderPosition;
+
+  const hideHeaderOnScroll = () => {
+    lastHeaderPosition = window.scrollY;
+    addClassItem('.header', 'scroll');
+
+    if (headerHeight - 50 < lastHeaderPosition && lastHeaderPosition > newHeaderPosition && newHeaderPosition !== 0) {
+      addClassItem('.header', 'hide');
+      addClassItem('.header', 'scroll');
+      } else if ((newHeaderPosition > lastHeaderPosition) && lastHeaderPosition !== 0 || lastHeaderPosition < headerHeight) {
+        removeClassItem('.header', 'hide');
+      }
+
+      if (lastHeaderPosition < headerHeight) removeClassItem('.header', 'scroll');
+
+      newHeaderPosition = lastHeaderPosition;
+  };
+
+  const throttle = (callback, delay) => {
+    let previousCall = new Date().getTime();
+      return function () {
+        const time = new Date().getTime();
+        if ((time - previousCall) >= delay) {
+          previousCall = time;
+          callback.apply(null, arguments);
         }
-
-        if (lastHeaderPosition < headerHeight) removeClassItem('.header', 'scroll');
-
-        newHeaderPosition = lastHeaderPosition;
-    };
-
-    const throttle = (callback, delay) => {
-        let previousCall = new Date().getTime();
-        return function () {
-            const time = new Date().getTime();
-            if ((time - previousCall) >= delay) {
-                previousCall = time;
-                callback.apply(null, arguments);
-            }
-        };
+      };
     }
 
     const throttleHideHeader = throttle(() => {
-        hideHeaderOnScroll();
+      hideHeaderOnScroll();
     }, 100);
 
     window.addEventListener('scroll', throttleHideHeader);
@@ -86,9 +89,32 @@ document.addEventListener('DOMContentLoaded', function(){
 
     if (window.matchMedia('(max-width: 787px)').matches) {
       burgerBtn.addEventListener('click', function(){
-        this.classList.toggle('open')
-        headerMenu.classList.toggle('open')
+        this.classList.toggle('open');
+        headerMenu.classList.toggle('open');
+        pageOverlay.classList.toggle('active');
+        page.classList.toggle('overflow');
       })
+
+      headerMenu.addEventListener('click', (event) => {
+        if (event.target.closest('a.menu__link')) {
+          removeBurgerMenu();
+        }
+      });
+
+      pageOverlay.addEventListener('click', (event) => {
+        removeBurgerMenu(this);
+      })
+
+      function removeBurgerMenu (targetEl) {
+        if (headerMenu.classList.contains('open')) {
+        burgerBtn.classList.remove('open');
+        headerMenu.classList.remove('open');
+        pageOverlay.classList.remove('active');
+        page.classList.remove('overflow');
+        }
+      }
+
+
     }
 
   //tabs with projects
